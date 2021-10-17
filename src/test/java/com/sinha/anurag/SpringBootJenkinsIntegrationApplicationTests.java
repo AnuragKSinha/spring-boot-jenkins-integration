1e1ad75d03b17b1399499e8ff66fd1d29aa2e0ec
Anurag Sinha <anuragsinha@Anurags-MacBook-Pro.local>
2021-10-17 17:51:45 +0530

    push

---
+++ src/test/java/com/sinha/anurag/SpringBootJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinha.anurag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
